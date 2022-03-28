308565be28b12c05d65cbb365f0cac41e1a8dd83
justvanrossum <justvanrossum@gmail.com>
2022-03-28 17:32:03 +0200

    add build.sh and reqs file

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e  # make sure to abort on error
+set -x  # echo commands
+
+
+fontmake -g sources/ColrV1_source.glyphs -o variable --output-dir build
+
+# python scripts/add_colrv1.py build/Colrv1Test-Regular-VF.ttf
