996fb1bc9c6bf6d572cbb77aae3fd0d035abec4d
justvanrossum <justvanrossum@gmail.com>
2022-03-28 17:37:30 +0200

    write fonts to 'fonts'

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -e  # make sure to abort on error
 set -x  # echo commands
 
 
-fontmake -g sources/ColrV1_source.glyphs -o ttf --output-dir build --master-dir build
+fontmake -g sources/ColrV1_source.glyphs -o ttf --output-dir fonts --master-dir build
 
 # python scripts/add_colrv1.py build/Colrv1Test-Regular-VF.ttf
