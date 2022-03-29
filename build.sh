3d11943cb585f490ffd57f8ef8eb6221d85c78ae
justvanrossum <justvanrossum@gmail.com>
2022-03-29 09:43:15 +0200

    fiddling with build

--- build.sh
+++ build.sh
@@ -4,6 +4,10 @@ set -e  # make sure to abort on error
 set -x  # echo commands
 
 
-fontmake -g sources/ColrV1_source.glyphs -o ttf --output-dir fonts --master-dir build
+python -m glyphsLib glyphs2ufo --output-dir build sources/ColrV1_source.glyphs --designspace-path /dev/null
+
+# python scripts/extrude_and_project.py build/Colrv1Test-Regular.ufo
+
+fontmake -u build/Colrv1Test-Regular.ufo -o ttf --output-dir fonts #--master-dir build
 
 # python scripts/add_colrv1.py build/Colrv1Test-Regular-VF.ttf
