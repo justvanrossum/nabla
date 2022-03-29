ae6982e91c13bd7f1182b0efabcaa2ab4b106764
justvanrossum <justvanrossum@gmail.com>
2022-03-29 20:59:19 +0200

    rename main script

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ set -x  # echo commands
 
 python -m glyphsLib glyphs2ufo --output-dir build sources/ColrV1_source.glyphs --designspace-path /dev/null
 
-python scripts/extrude_and_project.py build/Colrv1Test-Regular.ufo
+python scripts/shear_and_extrude.py build/Colrv1Test-Regular.ufo
 
 fontmake -m build/Colrv1Test-Regular.designspace -o variable --output-dir fonts
