623766a08cdc6165e1dd2b2d17e0f8be1ff6d0d2
justvanrossum <justvanrossum@gmail.com>
2022-03-31 13:29:09 +0200

    add wachter script, that triggers the build on changes in the .glyphs file

---
+++ auto_build.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+#
+# This script listens to changes in the "sources" folder,
+# and triggers the build script.
+# Keep the compiled font from the "fonts" folder open in
+# FontGoggles, and watch the saved changes from Glyphs.app
+# take effect.
+# 
+
+import subprocess
+from watchfiles import watch
+
+
+for changes in watch("Sources"):
+    print("Rebuilding font...")
+    result = subprocess.run(
+        ["./build.sh"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        encoding="utf-8",
+    )
+    if result.returncode != 0:
+        print(result.stdout)
+    print("Done.")
