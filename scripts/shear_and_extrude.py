a53f0b44cab62be11ad848fe129b95e8b8f83ec0
Just van Rossum <justvanrossum@gmail.com>
2022-08-12 10:13:43 +0200

    Disable stylistic sets for now

--- scripts/shear_and_extrude.py
+++ scripts/shear_and_extrude.py
@@ -510,14 +510,14 @@ def shearAndExtrude(path):
             extrudedFont.lib[COLOR_PALETTES_KEY] = palettes
             extrudedFont.lib[COLOR_LAYERS_KEY] = colorGlyphs
             extrudedFont.features.text += manualFeatures
-            extrudedFont.features.text += buildFeatures(
-                sorted(extrudedFont.keys()),
-                [
-                    ("ss01", frontSuffix, "Front"),
-                    ("ss02", sideSuffix, "Side"),
-                    ("ss03", highlightSuffix, "Highlight"),
-                ],
-            )
+            # extrudedFont.features.text += buildFeatures(
+            #     sorted(extrudedFont.keys()),
+            #     [
+            #         ("ss01", frontSuffix, "Front"),
+            #         ("ss02", sideSuffix, "Side"),
+            #         ("ss03", highlightSuffix, "Highlight"),
+            #     ],
+            # )
 
         extrudedPath = path.parent / (path.stem + "-" + depthName + path.suffix)
         extrudedFont.save(extrudedPath, overwrite=True)
